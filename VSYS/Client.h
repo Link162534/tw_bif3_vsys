99660f5779e9ca55774337c0cf9e79daf1725242
voki <voki@Voki-PC>
2016-11-07 22:56:35 +0100

    put works

--- VSYS/Client.h
+++ VSYS/Client.h
@@ -28,34 +28,49 @@
 #include <algorithm>
 #include <fstream>
 #include <list>
+#include <termios.h>
+#include <fstream>
+#include <fcntl.h>
 
 class Client {
 public:
     Client();
     Client(const Client& orig);
-    virtual ~Client();    
+    virtual ~Client();
     static void sighandler(int signum);
     int openConnection(char* ip, int port);
     void closeConnection();
     void printMenu();
     void onExit();
     void listenToInput();
+    int requestLogin();
 private:
     char* flag = buffer;
     char* message = buffer + 1;
+
     char* ip;
     int port;
     int socketID;
-    char buffer[BUFFER_SIZE+1];
+    char buffer[BUFFER_SIZE + 1];
     struct sockaddr_in address;
     int size;
     void list();
     void get(std::string name);
-    void put(char * filename);//param?
-    struct listObject{std::string filename; int size; };
+    void put(std::string name); //param?
+
+    struct listObject {
+        std::string filename;
+        int size = 0;
+    };
     std::list<listObject> fileList;
     char* getSecondStringFromArray(char * array);
     void printFileList();
+    void clearArray(char array[], int size);
+    std::string usingMessage;
+    bool fileExists(const char* name);
+    void sendFileSize(int& filesize);
+    void sendFilePart(int size);
+    int getFileSize(std::string filename);
 };
 
 #endif /* CLIENT_H */
--- VSYS/main.cpp
+++ VSYS/main.cpp
@@ -18,22 +18,19 @@ void sigHandle(int signum){
 
 
 int main(int argc, char** argv) {
-//    char name[10] = "1234\0lal"; 
-//    std::string newNAme(name);
-//    int lol = std::stoi(newNAme);
-//    std::cout << lol;
-    
-     //signal(SIGINT, sighandler);
     client = new Client();
     char* ip = argv[1];
     int port = atoi(argv[2]);
     if( client->openConnection(ip, port) == EXIT_FAILURE){
         return -1;
     }
+//    if(client->requestLogin() == -1)
     client->printMenu();
     client->listenToInput();
     signal(SIGINT, sigHandle);
     client->closeConnection();
     return 0;
+    
+
 }
 
